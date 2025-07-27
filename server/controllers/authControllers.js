36424622533fac3bb085a8841e7bf395b14414a8
dab22 <122424049+dab22@users.noreply.github.com>
2025-07-27 12:28:50 +0530

    Update authControllers.js

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const Admin = require("../models/Admin");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const signup = async (req, res) => {
